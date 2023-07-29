970756ce5b2d9f2318f6788dc28737bbebcf4ed0
idagelic <dagelic.ivan@gmail.com>
2023-07-29 09:50:07 +0200

    fix desc

--- Formula/daytona.rb
+++ Formula/daytona.rb
@@ -1,17 +1,17 @@
 class Daytona < Formula
-  desc "Daytona CLI for workspace management"
-  homepage "https://daytona.io/" # The URL to the project's homepage
-  url "https://github.com/idagelic/homebrew-dtn/archive/v0.1.0-alpha.tar.gz" # Replace with the download URL for your release
-  sha256 "44d1e4edbd9bbb969e3f9d7b60ab64216d932fa3620dc507e87ddf270bf3c1e7" # You can generate this with 'shasum -a 256 your-cli-X.X.X.tar.gz'
-  license "MIT" # Replace with the appropriate license for your CLI
+  desc "This is a Daytona CLI for workspace management"
+  homepage "https://daytona.io/"
+  url "https://github.com/idagelic/homebrew-dtn/archive/v0.1.0-alpha.tar.gz"
+  sha256 "44d1e4edbd9bbb969e3f9d7b60ab64216d932fa3620dc507e87ddf270bf3c1e7"
+  license "MIT"
 
   depends_on "go" => :build
 
   def install
     # Install the pre-built binary directly into the Homebrew 'bin' directory
-    bin.install "homebrew-dtn-0.1.0-alpha" => "daytona" # Rename the binary to 'your-cli' in the 'bin' directory
+    bin.install "homebrew-dtn-0.1.0-alpha" => "daytona"
     # system "go", "build", "-o", bin/"your-cli"
-    # Additionally, you can install any necessary files, such as documentation or completion scripts.
+    # Additionally, you can install any necessary files, such as documentation or completion.
     # For example:
     # doc.install "docs/readme.txt"
     # bash_completion.install "completion/bash-completion.sh"
