39356a3b3d08c39b54d2186cabc2108abce50798
idagelic <dagelic.ivan@gmail.com>
2023-07-29 13:03:36 +0200

    define resource prebuilt-binary

--- Formula/daytona.rb
+++ Formula/daytona.rb
@@ -8,14 +8,14 @@ class Daytona < Formula
   depends_on "go" => :build
 
   # Define the resource named "daytona"
-  resource "daytona" do
+  resource "prebuilt-binary" do
     url "https://github.com/idagelic/homebrew-dtn/archive/v0.1.0-alpha.tar.gz"
     sha256 "44d1e4edbd9bbb969e3f9d7b60ab64216d932fa3620dc507e87ddf270bf3c1e7"
   end
 
   def install
     # Use the pre-built binary from the resource
-    bin.install resource("daytona").files("daytona") # Replace 'daytona' with the actual binary name if different
+    bin.install resource("prebuilt-binary").files("daytona") # Replace 'daytona' with the actual binary name if different
     chmod "+x", "#{bin}/daytona" # Set execute permissions (if needed)
   end
 
